25e5663745f995c29db6da9e6867dad1e6d6a052
kostiantyn <kostiantyn@test.com>
2023-06-19 21:07:27 -0230

    first commit

--- src/App.js
+++ src/App.js
@@ -1,8 +1,13 @@
 import React from "react";
 import "./App.css";
+import ContainerCarousel from "./components/ContainerCarousel";
 
 function App() {
-  return;
+  return (
+    <>
+      <ContainerCarousel />
+    </>
+  );
 }
 
 export default App;
