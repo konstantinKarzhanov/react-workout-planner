080b36eee1e4c58f00997f0ba3f8779e753de288
Matthew Davis <86232146+Mattdavis80@users.noreply.github.com>
2023-06-19 19:04:40 -0230

    Updates to Header portion.

---
+++ src/components/Header.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <>
+      <div id="main-header">
+        <p id="header-text">Workout Planner</p>
+      </div>
+    </>
+  );
+};
+
+export default Header;
