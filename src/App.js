c351c7f7501eda4c1de289d99cfa3ddb11652d6f
Matthew Davis <86232146+Mattdavis80@users.noreply.github.com>
2023-06-21 23:25:04 -0230

    Added BodyPart Comps, updated CSS.

--- src/App.js
+++ src/App.js
@@ -1,14 +1,28 @@
-import React from "react";
+import React, { useEffect } from "react";
 import "./App.css";
 import Header from "./components/Header";
 import MainText from "./components/MainText";
+import BodyPartList from "./components/BodyPartList";
 import GenerateExercisesButton from "./components/GenerateExercisesButton";
 import ClearButton from "./components/ClearButton";
+// import { exerciseOptions, fetchData } from "./components/functions/fetchData";
+
+// // This is testing how the code should work
+// const fetchExerciseList = async () => {
+//   const list = await fetchData(
+//     "https://exercisedb.p.rapidapi.com/exercises/bodyPartList",
+//     exerciseOptions
+//   );
+//   // console.log(list);
+// };
 
 const App = () => (
   <>
     <Header />
     <MainText />
+    <BodyPartList />
+    <GenerateExercisesButton />
+    <ClearButton />
   </>
 );
 
