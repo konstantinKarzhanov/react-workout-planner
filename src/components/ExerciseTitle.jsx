fe42c3306b40ead095a2a5510b86ceed84ad6a1f
Matthew Davis <86232146+Mattdavis80@users.noreply.github.com>
2023-06-29 11:29:08 -0230

    css updates

---
+++ src/components/ExerciseTitle.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const ExerciseTitle = ({ title }) => {
+  return <h3 className="text-center flex flex--jc-c flex--ai-c">{title}</h3>;
+};
+
+export default ExerciseTitle;
